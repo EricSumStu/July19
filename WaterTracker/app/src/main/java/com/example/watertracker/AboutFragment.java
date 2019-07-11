6b1dfdb0a89b6ca0706eb1fd924d607f7841636b
sinead <sinead.flynn@ericsson.com>
2019-07-11 13:52:28 +0100

    Added water facts + button

--- WaterTracker/app/src/main/java/com/example/watertracker/AboutFragment.java
+++ WaterTracker/app/src/main/java/com/example/watertracker/AboutFragment.java
@@ -1,9 +1,11 @@
 package com.example.watertracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +17,20 @@ public class AboutFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable
             ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragmet_about, container, false);
+
+        ImageButton btn = (ImageButton) v.findViewById(R.id.waterFactsBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(getActivity(), waterFacts.class);
+
+                startActivity(intent1);
+            }
+        });
+
         return v;
+
+
+
     }
 }
---
+++ WaterTracker/app/src/main/java/com/example/watertracker/waterFacts.java
@@ -0,0 +1,14 @@
+package com.example.watertracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class waterFacts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water_facts);
+    }
+}
