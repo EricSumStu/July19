7c7bd151a91f35ee63b094f9af4419a4c64e4d45
sinead <sinead.flynn@ericsson.com>
2019-07-17 11:57:16 +0100

    waterlog

--- WaterTracker/app/src/main/java/com/example/watertracker/HomeFragment.java
+++ WaterTracker/app/src/main/java/com/example/watertracker/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.watertracker;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -21,6 +22,9 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -41,6 +45,7 @@ public class HomeFragment extends Fragment {
         ImageButton bottleImageButton = (ImageButton) v.findViewById(R.id.bottlebtn);
         ImageButton glassImageButton = (ImageButton) v.findViewById(R.id.glassbtn);
         ImageButton resetImageButton = (ImageButton) v.findViewById(R.id.resetbtn);
+        ImageButton iconButton = (ImageButton) v.findViewById(R.id.logicon);
         mTextView = (TextView) v.findViewById(R.id.countertext);
         waterTracker = (ProgressBar) v.findViewById(R.id.waterCounter);
         waterTracker.setMax(3000);
@@ -70,6 +75,19 @@ public class HomeFragment extends Fragment {
                 mTextView.setText("Total ml: " + counter);
                 waterTracker.setProgress(counter);
                 save(v);
+
+            }
+        });
+
+        iconButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    log();
+                    System.out.println("Working");
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
 
             }
         });
@@ -172,5 +190,35 @@ public class HomeFragment extends Fragment {
             }
         }
     }
+
+    public void log() throws FileNotFoundException {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = new Date();
+
+
+        final String FILE_NAME = "waterstats.txt";
+
+        String text = Integer.toString(counter)+ "," + dateFormat.format(date);
+        FileOutputStream fos = new FileOutputStream(FILE_NAME,true);
+
+        try {
+            fos = getActivity().openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+
+            Toast.makeText(getActivity(), "Logged",
+                    Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } }
 
 }
--- WaterTracker/app/src/main/java/com/example/watertracker/StatFragment.java
+++ WaterTracker/app/src/main/java/com/example/watertracker/StatFragment.java
@@ -4,17 +4,67 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class StatFragment extends Fragment {
+    private String info;
+    private static final String FILE_NAME = "waterstats.txt";
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable
             ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_stat, container, false);
+         TextView mTextView = (TextView) v.findViewById(R.id.log_text);
+
+
+        load(v);
+
+        mTextView.setText(info);
+
+
         return v;
+    }
+
+    public void load (View v){
+        FileInputStream fis = null;
+
+        try {
+            fis = getActivity().openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+
+            while ((text = br.readLine()) != null) {
+                sb.append(text);
+            }
+
+            info = sb.toString();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
