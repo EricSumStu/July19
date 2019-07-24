746f10423b1e907c64b0dfc4d9bbb12cc133fe42
annaconnolly <anna.connolly@ericsson.com>
2019-07-24 17:12:35 +0100

    About Button Delay

--- WaterTracker/app/src/main/java/com/example/watertracker/AboutFragment.java
+++ WaterTracker/app/src/main/java/com/example/watertracker/AboutFragment.java
@@ -2,9 +2,12 @@ package com.example.watertracker;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
@@ -12,28 +15,58 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class AboutFragment extends Fragment {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable
             ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragmet_about, container, false);
+        final ImageButton factsbtn = (ImageButton) v.findViewById(R.id.waterFactsBtn);
+        final Animation dropAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.bounce);
+        final Animation peopleAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.bounce);
+        MyBounceInterpolator interpolator = new MyBounceInterpolator(0.2, 20);
+        dropAnimation.setInterpolator(interpolator);
+        factsbtn.setAnimation(dropAnimation);
 
-        ImageButton factsbtn = (ImageButton) v.findViewById(R.id.waterFactsBtn);
-        ImageButton credits = (ImageButton) v.findViewById(R.id.creditsbtn);
+        final ImageButton credits = (ImageButton) v.findViewById(R.id.creditsbtn);
+        peopleAnimation.setInterpolator(interpolator);
+        credits.setAnimation(peopleAnimation);
         factsbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent1 = new Intent(getActivity(), waterFacts.class);
+                factsbtn.startAnimation(dropAnimation);
 
-                startActivity(intent1);
+
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent mainIntent = new Intent(getActivity(), waterFacts.class);
+                        getActivity().startActivity(mainIntent);
+                        getActivity().finish();
+
+                    }
+                }, SPLASH_DISPLAY_LENGTH);
             }
         });
         credits.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent2 = new Intent(getActivity(), creditsPage.class);
+                credits.startAnimation(peopleAnimation);
 
-                startActivity(intent2);
+
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent mainIntent = new Intent(getActivity(), creditsPage.class);
+                        getActivity().startActivity(mainIntent);
+                        getActivity().finish();
+
+                    }
+                }, SPLASH_DISPLAY_LENGTH);
             }
         });
 
